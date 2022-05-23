8abc9cf4a7c826cdce0ef13c9e83baac845ca165
Evi Owhotemu <evicoach@gmail.com>
2022-05-23 16:51:58 +0100

    Updated duration test

--- src/tests/time.test.ts
+++ src/tests/time.test.ts
@@ -1,30 +1,51 @@
-import { expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import TimeUtil from "../time/time_util";
 import { Duration } from "../util/types";
 
-it("should check two given times for coincidence", () => {
-  const duration1: Duration = {
-    start: {
-      hour: 0,
-      minute: 11,
-    },
-    end: {
-      hour: 14,
-      minute: 0,
-    },
-  };
 
-  const duration2: Duration = {
-    start: {
-      hour: 0,
-      minute: 0,
-    },
-    end: {
-      hour: 15,
-      minute: 12,
-    },
-  };
-    
+describe("TimeUtil.parseDuration(...)", () => {
+  it("should convert string duration to duration object", () => {
+    const duration = "10:00-12:00";
+    const expectedResult = {
+      start: {
+        hour: 10,
+        minute: 0,
+      },
+      end: {
+        hour: 12,
+        minute: 0,
+      },
+    };
+    const result = TimeUtil.parseDuration(duration);
+    expect(result).toStrictEqual(expectedResult);
+})
+});
+
+describe("TimeUtil.checkCoincidence(...)", () => {
+  it("should check two given times for coincidence", () => {
+    const duration1: Duration = {
+      start: {
+        hour: 0,
+        minute: 11,
+      },
+      end: {
+        hour: 14,
+        minute: 0,
+      },
+    };
+
+    const duration2: Duration = {
+      start: {
+        hour: 0,
+        minute: 0,
+      },
+      end: {
+        hour: 15,
+        minute: 12,
+      },
+    };
+
     const result = TimeUtil.checkCoincidence(duration1, duration2);
     expect(result).toBe(true);
+  });
 });
